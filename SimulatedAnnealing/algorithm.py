c3dcb7268d7580ea38b44802889e179f4f4afbdc
Ana <acpagnoncelli@inf.ufrgs.br>
2021-05-15 23:37:44 -0300

    variables by parameter

--- SimulatedAnnealing/algorithm.py
+++ SimulatedAnnealing/algorithm.py
@@ -2,16 +2,20 @@ import sys
 import numpy as np
 import time
 from random import random, randint, choice
+import random
 from generateMatrix import colToMatrix
 
 ALPHA = 0.9
 MIN_TEMPERATURE = 0.001
 INITIAL_TEMPERATURE = 10.0
-MAX_ITERATIONS = 100
+MAX_ITERATIONS = 15000
+FILE_NAME = '2-FullIns_3'
+SEED = 3
 
-instance_list = ['2-FullIns_3.col', '2-FullIns_4.col', '4-FullIns_3.col', '5-FullIns_3.col', 'queen5_5.col', 'queen6_6.col', 'queen7_7.col', 'queen9_9.col', 'queen10_10.col', 'queen11_11.col']
+instance_list = [FILE_NAME + '.col']
 
 # MAIN PROGRAM
+
 
 def color_graph(vertices, matrix):
     cost = 0
@@ -39,7 +43,7 @@ def simulated_annealing(vertices_number, matrix):
             new_cost = get_cost(new_solution, vertices_number)
             vertice = get_next_vertice(vertices_number)
             accept = acceptance(cost, new_cost, temperature)
-            if accept > random():
+            if accept > random.random():
                 solution = new_solution.copy()
                 cost = new_cost
                 colors_number = cost + 1
@@ -59,7 +63,7 @@ def acceptance(old_cost, new_cost, temperature):
 
 
 def get_next_vertice(vertices_number):
-    return randint(0, vertices_number - 1)
+    return random.randint(0, vertices_number - 1)
 
 
 def get_cost(solution, vertices_number):
@@ -75,9 +79,9 @@ def get_cost(solution, vertices_number):
 def generate_first_solution(vertices_number, colors_number, matrix):
     solution = np.arange(vertices_number)
     for i in range(0, vertices_number):
-        solution[i] = randint(0, colors_number)
+        solution[i] = random.randint(0, colors_number)
         while(not is_valid_color(solution, i, vertices_number, matrix)):
-            solution[i] = randint(0, colors_number)
+            solution[i] = random.randint(0, colors_number)
     return solution
 
 
@@ -91,10 +95,10 @@ def is_valid_color(solution, vertice, vertices_number, matrix):
 def generate_new_solution(solution, colors_number, vertice, vertices_number, matrix):
     new_solution = solution.copy()
     old_color = new_solution[vertice]
-    new_solution[vertice] = randint(0, colors_number)
+    new_solution[vertice] = random.randint(0, colors_number)
     i = 0
     while(not is_valid_color(new_solution, vertice, vertices_number, matrix) and (i < colors_number)):
-        new_solution[vertice] = randint(0, colors_number)
+        new_solution[vertice] = random.randint(0, colors_number)
         i = i + 1
 
     if (i == colors_number):
@@ -104,6 +108,15 @@ def generate_new_solution(solution, colors_number, vertice, vertices_number, mat
 
 # Execute Program
 if __name__ == "__main__":
+    FILE_NAME = sys.argv[1]
+    INITIAL_TEMPERATURE = float(sys.argv[2])
+    ALPHA = float(sys.argv[3])
+    MAX_ITERATIONS = int(sys.argv[4])
+    MIN_TEMPERATURE = float(sys.argv[5])
+    SEED = int(sys.argv[6])
+
+    random.seed(SEED)
+
     results_file = open("results.txt", "w")
 
     results_file.write(f"ALPHA = {ALPHA}\n")
@@ -115,7 +128,7 @@ if __name__ == "__main__":
         # Translates instance to a usable matrix
         matrix, vertices = colToMatrix(instance)
         best_result = color_graph(vertices, matrix)
-        
+
         results_file.write(f"{instance}: {best_result}\n")
-    
+
     results_file.close()
